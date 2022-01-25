a46339a61ecbebeaf0e9339b4ab22b9c4ca682b1
Cameron Cianci <Cameron.cianci@gmail.com>
2022-01-25 14:59:28 -0500

    Added Improved Action

--- MetropolisSHO.py
+++ MetropolisSHO.py
@@ -3,37 +3,44 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 global Ncf
-Ncf = 10000
+Ncf = 20000
 global N_cor 
 N_cor = 20
 global N
 N = 20
 global a
 a = .5
+global binsize
+binsize = 1
+HighPrecision = False
 
-def findSigma(G,sigma): #find sigma^2(input is an array of all configuration)
+def findSigma(G,sigma): #find uncertainty estimate(input is an array of all configuration)
     global Ncf
+    global binsize
+    newG = bin(G,binsize)
     for n in range(0,N):
         sigmaSquared = 0
-        for i in range(Ncf):
-            sigmaSquared += G[i][n]**2
-        sigmaSquared = sigmaSquared/Ncf
+        for i in range(0,int(Ncf/binsize)):
+            sigmaSquared += newG[i][n]**2
+        sigmaSquared = sigmaSquared/(Ncf/binsize)
         myVar = 0
-        for i in range(Ncf):  #find expectation value of Gamma(GammaBar)
-            myVar += G[i][n]
-        myVar = myVar/Ncf
+        for i in range(0,int(Ncf/binsize)):  #find expectation value of Gamma(GammaBar)
+            myVar += newG[i][n]
+        myVar = myVar/(Ncf/binsize)
         myVar = myVar**2
         sigmaSquared = sigmaSquared - myVar
-        sigmaSquared = sigmaSquared/Ncf
+        sigmaSquared = sigmaSquared/(Ncf/binsize)
         sigma[n] = math.sqrt(sigmaSquared)
-    print(sigma)
 
-def S(i,x):
+def S(i,x):#finds the action
     global a
     global N
-    return a*x[i]**2/2 + x[i]*(x[i]-x[(i+1)%N]-x[(i-1)%N])/a
+    if(HighPrecision == True):
+        return x[i]*(x[i]-x[(i+1)%N]-x[(i-1)%N])/(a) + x[i]*(-x[(i+2)%N]+4*x[(i+1)%N]-3*x[i]+4*x[(i-1)%N]-x[(i-2)%N])/(12*a) + a*x[i]**2/2
+    else:
+        return x[i]*(x[i]-x[(i+1)%N]-x[(i-1)%N])/(a) + a*x[i]**2/2
 
-def update(x):
+def update(x):#updates the configuration
     eps = 1.4
     for i in range(0,N):
         old_x = x[i]
@@ -50,11 +57,11 @@ def compute_G(x,n):
         g = g + x[i]*x[(i+n)%N]
     return g/N
 
-def Thermalize(x):
+def Thermalize(x):#initializes the lattice
     for i in range(0, 5*N_cor):
         update(x)
  
-def Run(x,G,y):
+def Run(x,G,y):#runs the MC simulation
     for alpha in range(0,Ncf):
         for j in range(0,N_cor):
             update(x)
@@ -64,11 +71,13 @@ def Run(x,G,y):
     
 def MCaverage(x,G,avg_G):
     global N
+    global binsize
+    newG = bin(G,binsize)
     for n in range(0,N):
         avg_G[n] = 0
-        for alpha in range(0,Ncf):
-            avg_G[n] += G[alpha][n]
-        avg_G[n] = avg_G[n]/Ncf
+        for alpha in range(0,int(Ncf/binsize)):
+            avg_G[n] += newG[alpha][n]
+        avg_G[n] = avg_G[n]/(Ncf/binsize)
         #print("G(%d) = %g" % (n,avg_G[n]))
 
 def DeltaE(avg_G,sigma):#throws error when i+1 is negative
@@ -77,14 +86,40 @@ def DeltaE(avg_G,sigma):#throws error when i+1 is negative
     global a
     DelE = [0 for n in range(0,N)]
     for n in range(0,N):
-        DelE[n] = math.log(avg_G[n]/avg_G[(n+1)%N])/a
+        DelE[n] = math.log(abs(avg_G[n]/avg_G[(n+1)%N]))/a
     fig, ax = plt.subplots()
     ax.errorbar([x/2 for x in range(N)], DelE, yerr=sigma, fmt='go')
-    ax.set_xlim([0, 2])
+    #ax.set_xlim([0, 2])
     ax.set_xlabel('t')
     ax.set_ylabel('Energy')
     ax.set_title('Quantum SHO Simulation of Delta E = (E_0 - E_1), N_cf = %d' % (Ncf))
     plt.show()
+
+def boostrap(G):#creates bootstrap copies of ensembles to estimate error
+    Ncf = len(G)
+    G_bootstrap = []
+    for i in range(0,Ncf):
+        alpha = int(np.random.uniform(0,Ncf))
+        G_bootstrap.append(G[alpha])
+    return G_bootstrap
+
+def bin(G,binsize):#bins G to save space
+    G_binned = []
+    for i in range(0,len(G),binsize):
+        G_avg = [0 for k in range(0,N)]
+        for j in range(0,binsize):
+            for n in range(0,N):
+                G_avg[n] = G_avg[n] + G[i+j][n]/binsize
+        G_binned.append(G_avg)
+    return G_binned
+
+def avg_y(y):#averages y to attempt to find the approximate wavefunction
+    avg = [0 for i in range(0,N)]
+    for n in range(0,N):
+        for alpha in range(0,Ncf):
+            avg[n] += y[alpha][n]
+        avg[n] = avg[n]/Ncf
+    return avg
 
 def main():
     y = [[0 for n in range(0,N)] for alpha in range(0,Ncf)]
@@ -94,8 +129,16 @@ def main():
     sigma = [0 for n in range(0,N)]
     Thermalize(x)
     Run(x,G,y)
-    MCaverage(x,G, avg_G)
-    findSigma(G,sigma)
-    DeltaE(avg_G,sigma)
+    if(1==1):#finds Delta_E
+        MCaverage(x,G, avg_G)
+        findSigma(G,sigma)
+        DeltaE(avg_G,sigma)
+    else: #shows average wavefunction found
+        fig, ax = plt.subplots()
+        ax.errorbar([x/2 for x in range(N)], avg_y(y), fmt='go')
+        ax.set_xlabel('t')
+        ax.set_ylabel('Energy')
+        ax.set_title('Quantum SHO Simulation of Delta E = (E_0 - E_1), N_cf = %d' % (Ncf))
+        plt.show()
 
 main()
